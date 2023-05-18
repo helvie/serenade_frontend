ffe9f0931c5370c352afddd68acdc7db5eebf3e7
florent <lan.ngongang@gmail.com>
2023-05-18 14:11:27 +0200

    start home

---
+++ fakeData/allUsers.js
@@ -0,0 +1,98 @@
+export const allUsers = [
+  {
+    id: 1,
+    firstname: "John",
+    city: "New York",
+    age: 30,
+    gender: "Male",
+    sexuality: "Heterosexual",
+    jobTitle: "Software Engineer",
+    description:
+      "Experienced software engineer proficient in multiple programming languages.",
+    pictures: [
+      "https://www.pexels.com/photo/person-wearing-blue-denim-jacket-and-black-denim-jeans-3157815/",
+      "https://www.pexels.com/photo/photo-of-woman-wearing-blue-denim-jacket-3184412/",
+      "https://www.pexels.com/photo/photo-of-woman-in-red-sweater-3182786/",
+    ],
+  },
+  {
+    id: 2,
+    firstname: "Emily",
+    city: "London",
+    age: 28,
+    gender: "Female",
+    sexuality: "Heterosexual",
+    jobTitle: "Graphic Designer",
+    description:
+      "Passionate graphic designer creating visually stunning designs.",
+    pictures: [
+      "https://www.pexels.com/photo/photo-of-woman-with-black-and-red-blazer-3173554/",
+      "https://www.pexels.com/photo/photo-of-woman-wearing-white-top-3173553/",
+      "https://www.pexels.com/photo/photo-of-woman-wearing-white-and-black-stripe-shirt-3182787/",
+    ],
+  },
+  {
+    id: 3,
+    firstname: "John",
+    city: "New York",
+    age: 30,
+    gender: "Male",
+    sexuality: "Heterosexual",
+    jobTitle: "Software Engineer",
+    description:
+      "Experienced software engineer proficient in multiple programming languages.",
+    pictures: [
+      "https://www.pexels.com/photo/person-wearing-blue-denim-jacket-and-black-denim-jeans-3157815/",
+      "https://www.pexels.com/photo/photo-of-woman-wearing-blue-denim-jacket-3184412/",
+      "https://www.pexels.com/photo/photo-of-woman-in-red-sweater-3182786/",
+    ],
+  },
+  {
+    id: 4,
+    firstname: "Emily",
+    city: "London",
+    age: 28,
+    gender: "Female",
+    sexuality: "Heterosexual",
+    jobTitle: "Graphic Designer",
+    description:
+      "Passionate graphic designer creating visually stunning designs.",
+    pictures: [
+      "https://www.pexels.com/photo/photo-of-woman-with-black-and-red-blazer-3173554/",
+      "https://www.pexels.com/photo/photo-of-woman-wearing-white-top-3173553/",
+      "https://www.pexels.com/photo/photo-of-woman-wearing-white-and-black-stripe-shirt-3182787/",
+    ],
+  },
+  {
+    id: 5,
+    firstname: "John",
+    city: "New York",
+    age: 30,
+    gender: "Male",
+    sexuality: "Heterosexual",
+    jobTitle: "Software Engineer",
+    description:
+      "Experienced software engineer proficient in multiple programming languages.",
+    pictures: [
+      "https://www.pexels.com/photo/person-wearing-blue-denim-jacket-and-black-denim-jeans-3157815/",
+      "https://www.pexels.com/photo/photo-of-woman-wearing-blue-denim-jacket-3184412/",
+      "https://www.pexels.com/photo/photo-of-woman-in-red-sweater-3182786/",
+    ],
+  },
+  {
+    id: 6,
+    firstname: "Emily",
+    city: "London",
+    age: 28,
+    gender: "Female",
+    sexuality: "Heterosexual",
+    jobTitle: "Graphic Designer",
+    description:
+      "Passionate graphic designer creating visually stunning designs.",
+    pictures: [
+      "https://www.pexels.com/photo/photo-of-woman-with-black-and-red-blazer-3173554/",
+      "https://www.pexels.com/photo/photo-of-woman-wearing-white-top-3173553/",
+      "https://www.pexels.com/photo/photo-of-woman-wearing-white-and-black-stripe-shirt-3182787/",
+    ],
+  },
+];
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import { View, Text, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 import SearchSettings from "./SearchSettings";
 import globalStyles from "../../utils/globalStyles";
+import Swiper from "react-native-deck-swiper";
+import { allUsers } from "../../fakeData/allUsers";
 
 const HomeScreen = () => {
   const [openSearchSettings, setOpenSearchSettings] = useState(false);
@@ -10,22 +12,37 @@ const HomeScreen = () => {
   };
   return (
     <View style={globalStyles.screen}>
-      <SearchSettings
-        openSearchSettings={openSearchSettings}
-        closeSearchSettings={closeSearchSettings}
-      />
       <View style={globalStyles.container}>
-        <TouchableOpacity
-          className="bg-black"
-          onPress={() => {
-            setOpenSearchSettings(true);
-          }}
-        >
-          <Text style={globalStyles.mainText}>HomeScreen</Text>
-        </TouchableOpacity>
+        {/* <HomeHeader /> */}
+        <View className="flex-1">
+          <Swiper
+            containerStyle={{ backgroundColor: "transparent" }}
+            cards={allUsers}
+            renderCard={(card) => (
+              <View key={card.id} className="bg-red-500">
+                <Text>{card.firstname}</Text>
+              </View>
+            )}
+          />
+        </View>
       </View>
     </View>
   );
 };
 
 export default HomeScreen;
+
+// <SearchSettings
+//   openSearchSettings={openSearchSettings}
+//   closeSearchSettings={closeSearchSettings}
+// />
+// <View style={globalStyles.container}>
+//   <TouchableOpacity
+//     className="bg-black"
+//     onPress={() => {
+//       setOpenSearchSettings(true);
+//     }}
+//   >
+//     <Text style={globalStyles.mainText}>HomeScreen</Text>
+//   </TouchableOpacity>
+// </View>
